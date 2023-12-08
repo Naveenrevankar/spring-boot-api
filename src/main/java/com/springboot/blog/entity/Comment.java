46a85c7aafc36653cca9cb3eae125665cd14e551
Naveen <naveenrevankar53@gmail.com>
2023-12-08 23:54:17 +0530

    welcome added

--- src/main/java/com/springboot/blog/entity/Comment.java
+++ src/main/java/com/springboot/blog/entity/Comment.java
@@ -14,6 +14,7 @@ import javax.persistence.*;
 @Table(name = "comments")
 public class Comment {
 
+//  this is the name of the ID of the comment
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
